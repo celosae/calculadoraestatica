11a1da19b8edcc7ca47a9ad960002366a26d9d9c
César López <clopezs@iescomercio.com>
2024-02-14 20:33:26 +0100

    Borrar clases no utilizadas: sumar, restar, multiplicar, dividir

--- src/calculadora/Calculadora.java
+++ src/calculadora/Calculadora.java
@@ -10,6 +10,24 @@ package calculadora;
  */
 
 public class Calculadora {
+    
+    private int a, b;
+
+    public int getA() {
+        return a;
+    }
+
+    public void setA(int a) {
+        this.a = a;
+    }
+
+    public int getB() {
+        return b;
+    }
+
+    public void setB(int b) {
+        this.b = b;
+    }
 
     public int sumar(int a, int b) {
         return a + b;
